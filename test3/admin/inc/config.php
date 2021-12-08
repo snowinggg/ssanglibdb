99326611a2ca9b380646fa079b40bb85f34d636b
shin <shin5513976@naver.com>
2021-12-08 14:39:21 +0900

    v0.3

--- test3/admin/inc/config.php
+++ test3/admin/inc/config.php
@@ -15,7 +15,7 @@ $dbname = 'fashiony_ogs';
 $dbuser = 'root';
 
 // Database Password
-$dbpass = 'root';
+$dbpass = 'qpfldhkdwk1*';
 
 // Defining base url
 define("BASE_URL", "http://fashionys.com/");
